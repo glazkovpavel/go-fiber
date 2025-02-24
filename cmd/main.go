58bc524a71720bb5a972d9845bb04e93e242901d
pglazkov <glazprom@mail.ru>
2025-02-24 10:03:04 +0300

    initial commit

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"go/go-fiber/config"
 	"go/go-fiber/internal/home"
+	"log"
 )
 
 func main() {
+	config.Init()
+	dbConf := config.NewDatabaseConfig()
+	log.Println(dbConf)
 	app := fiber.New()
 
 	home.NewHomeHandler(app)
