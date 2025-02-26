fd9186f33e60773f3c8681d0ab2c28c09e81950d
pglazkov <glazprom@mail.ru>
2025-02-26 21:17:26 +0300

    добавление логера

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"github.com/gofiber/contrib/fiberzerolog"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"go/go-fiber/config"
 	"go/go-fiber/internal/home"
-	"log"
+	"go/go-fiber/pkg/logger"
 )
 
 func main() {
 	config.Init()
-	dbConf := config.NewDatabaseConfig()
-	log.Println(dbConf)
+	config.NewDatabaseConfig()
+	logConfig := config.NewLogConfig()
+	customLogger := logger.NewLogger(logConfig)
+
 	app := fiber.New()
 
-	home.NewHomeHandler(app)
+	app.Use(fiberzerolog.New(fiberzerolog.Config{
+		Logger: customLogger,
+	}))
+	app.Use(recover.New()) // для того чтобы не падало приложение при панике
+
+	home.NewHomeHandler(app, customLogger)
 	app.Listen(":3000")
 }
--- internal/home/handler.go
+++ internal/home/handler.go
@@ -1,18 +1,30 @@
 package home
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/rs/zerolog"
+)
 
 type HomeHandler struct {
-	router fiber.Router
+	router       fiber.Router
+	customLogger *zerolog.Logger
 }
 
-func NewHomeHandler(router fiber.Router) {
+func NewHomeHandler(router fiber.Router, customLogger *zerolog.Logger) {
 	h := &HomeHandler{
-		router: router,
+		router:       router,
+		customLogger: customLogger,
 	}
-	h.router.Get("/", h.Home)
+	api := h.router.Group("/api")
+	api.Get("/", h.Home)
+	api.Get("/error", h.GetError)
 }
 
 func (h *HomeHandler) Home(c *fiber.Ctx) error {
+	h.customLogger.Info().Bool("isAdmin", true).Msg("Инфо")
 	return c.SendString("Hello, World!")
+}
+
+func (h *HomeHandler) GetError(c *fiber.Ctx) error {
+	return c.SendString("Error")
 }
